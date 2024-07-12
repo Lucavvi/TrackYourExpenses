a21b51194af6df359bf526a6212a03dc04d2da95
XxAngelo3040xX <116958065+XxAngelo3040xX@users.noreply.github.com>
2024-07-12 11:41:41 +0200

    Activated password mode, for password field

--- Avveduto-Ripamonti_TrackYourExpensive/src/form/FormView.java
+++ Avveduto-Ripamonti_TrackYourExpensive/src/form/FormView.java
@@ -23,7 +23,7 @@ class FormView {
         this.processing = processing;
         this.cp5 = cp5;
         usernameField = cp5.addTextfield("username").setColorForeground(processing.color(255)).setFont(processing.createFont("arial",25)).setPosition(processing.width/2-100,processing.height/2-40).setSize(200,40).setFocus(true).setColor(processing.color(255)).setColorActive(processing.color(255)).setColorBackground(0).setCaptionLabel("").hide();
-        passwordField = cp5.addTextfield("password").setColorForeground(processing.color(255)).setFont(processing.createFont("arial",25)).setPosition(processing.width/2-100,processing.height/2+40).setSize(200,40).setFocus(true).setColor(processing.color(255)).setColorActive(processing.color(255)).setColorBackground(0).setCaptionLabel("").hide();
+        passwordField = cp5.addTextfield("password").setPasswordMode(true).setColorForeground(processing.color(255)).setFont(processing.createFont("arial",25)).setPosition(processing.width/2-100,processing.height/2+40).setSize(200,40).setFocus(true).setColor(processing.color(255)).setColorActive(processing.color(255)).setColorBackground(0).setCaptionLabel("").hide();
         submitButton = cp5.addButton("submit").setLabel("Submit").setPosition(processing.width/2-40, processing.height/2+110).setSize(80, 30).hide();
     }
 
