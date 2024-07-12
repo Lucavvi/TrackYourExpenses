d9ed36949408cd9b782cf5005c05434b535aee47
Luke <l.avveduto00@gmail.com>
2024-07-12 12:30:45 +0200

    enum

--- Avveduto-Ripamonti_TrackYourExpensive/src/expense/Categories.java
+++ Avveduto-Ripamonti_TrackYourExpensive/src/expense/Categories.java
@@ -1,5 +1,11 @@
 package expense;
 import java.awt.*;
+
+/**
+ * Enum with all the categories
+ * @author luke & Angelo
+ * @version 1.0
+ */
 public enum Categories {
     FOOD(Color.GREEN,"Food"),
     SHOPPING(Color.ORANGE,"Shopping"),
@@ -7,13 +13,26 @@ public enum Categories {
 
     private Color color;
     private String name;
+
+    /**
+     * @param c - color
+     * @param n - name
+     * constructor
+     */
     private Categories(Color c,String n) {
         color = c;
         name = n;
     }
 
+    /**
+     * @return the category's color
+     */
     public Color getColor() {
         return color;
     }
+
+    /**
+     * @return the category's name
+     */
     public String getName() {return name;}
 }
