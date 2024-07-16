51a349e8ddb321d7e6d17793436fa0859de61585
XxAngelo3040xX <116958065+XxAngelo3040xX@users.noreply.github.com>
2024-07-16 15:37:17 +0200

    try

---
+++ Avveduto-Ripamonti_TrackYourExpensive/src/expense/LocalDate.java
@@ -0,0 +1,35 @@
+package expense;
+
+import java.io.Serializable;
+
+
+public class LocalDate implements Serializable {
+    private int day;
+    private int month;
+    private int year;
+    private boolean leapYear;
+
+    public LocalDate(java.time.LocalDate date){
+        day = date.getDayOfMonth();
+        month = date.getMonthValue();
+        year = date.getYear();
+        leapYear = date.isLeapYear();
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isLeapYear() {
+        return leapYear;
+    }
+
+}
