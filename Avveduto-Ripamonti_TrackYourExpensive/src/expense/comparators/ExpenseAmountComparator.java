ccce30bd5d6b7cf66499de3262023b391e9f09ec
Luke <l.avveduto00@gmail.com>
2024-07-20 11:16:38 +0200

    JD

--- Avveduto-Ripamonti_TrackYourExpensive/src/expense/comparators/ExpenseAmountComparator.java
+++ Avveduto-Ripamonti_TrackYourExpensive/src/expense/comparators/ExpenseAmountComparator.java
@@ -3,12 +3,26 @@ package expense.comparators;
 import expense.ExpenseController;
 import java.util.Comparator;
 
+/**
+ * Comparator by amount
+ * @author luke
+ * @version 1.0
+ */
 public class ExpenseAmountComparator implements Comparator<ExpenseController> {
+    /**
+     * @param expenseController - first obj
+     * @param t1 - second obj
+     * @return - conventional values
+     */
     @Override
     public int compare(ExpenseController expenseController, ExpenseController t1) {
         return Float.compare(expenseController.getModel().getAmount(),t1.getModel().getAmount());
     }
 
+    /**
+     * Reversed comparator
+     * @return reversed comparator
+     */
     @Override
     public Comparator<ExpenseController> reversed() {
         return Comparator.super.reversed();
