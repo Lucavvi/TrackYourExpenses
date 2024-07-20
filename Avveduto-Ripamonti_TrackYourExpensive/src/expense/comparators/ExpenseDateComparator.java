7126b930adc7b8080b4753b2cff2e834ffc4e71d
Luke <l.avveduto00@gmail.com>
2024-07-20 11:16:23 +0200

    jd

--- Avveduto-Ripamonti_TrackYourExpensive/src/expense/comparators/ExpenseDateComparator.java
+++ Avveduto-Ripamonti_TrackYourExpensive/src/expense/comparators/ExpenseDateComparator.java
@@ -3,7 +3,17 @@ package expense.comparators;
 import expense.ExpenseController;
 import java.util.Comparator;
 
+/**
+ * Comparator by date
+ * @author luke
+ * @version 1.0
+ */
 public class ExpenseDateComparator implements Comparator<ExpenseController> {
+    /**
+     * @param exp - first obj
+     * @param t1 - second obj
+     * @return - conventional values
+     */
     @Override
     public int compare(ExpenseController exp, ExpenseController t1) {
         if(exp.getModel().getDate().isBefore(t1.getModel().getDate())) return -1;
@@ -11,6 +21,10 @@ public class ExpenseDateComparator implements Comparator<ExpenseController> {
         else return 1;
     }
 
+    /**
+     * Reversed comparator
+     * @return reversed comparator
+     */
     @Override
     public Comparator<ExpenseController> reversed() {
         return Comparator.super.reversed();
